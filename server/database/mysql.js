8bbe0833e2b6a02d05632cf3b1d1a30038b79596
Sergio Aparicio <sergiomauricio111@gmail.com>
2020-10-14 16:32:11 -0500

    Se implemento el control de Usuarios parte 2

---
+++ server/database/mysql.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql');
+class MySQL {
+    constructor() {
+        this.conectado = false;
+        console.log('clase inicializada');
+        this.conexion = mysql.createConnection({
+            host: 'localhost',
+            user: 'sistemaDomicilio',
+            password: '123456',
+            database: 'sistemadomicilio',
+        });
+        this.conectarDB();
+    }
+    static get instance() {
+        return this._instance || (this._instance = new this());
+    }
+    static ejecutarQuery(query, callback) {
+        this.instance.conexion.query(query, (err, result, fields) => {
+            if (err) {
+                console.log('Error : ' + err);
+                return callback(err);
+            }
+            if (result.length == 0) {
+                callback('El registro solicitado no Existe');
+            } else {
+                callback(null, result);
+            }
+        });
+    }
+    conectarDB() {
+        this.conexion.connect((err) => {
+            if (err) {
+                console.log(err.message);
+                return;
+            }
+            this.conectado = true;
+            console.log('Base de Datos Conectada con Exito');
+        });
+    }
+}
+module.exports = {
+    MySQL,
+};
